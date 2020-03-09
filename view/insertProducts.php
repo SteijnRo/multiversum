d47ed607663e23046b834ac4ff6895a7c50fca67
Gizoisa <534677@edu.rocmn.nl>
2020-03-09 15:05:02 +0100

    frontend  that i made for you u lazy fuck

--- view/insertProducts.php
+++ view/insertProducts.php
@@ -2,35 +2,39 @@
 include_once "view/header.php";
 ?>
 <div class="album py-5" id="main">
-  <div class="container">
+<form action="?op=addProduct" method="post" style="margin:50px;" >
+  <div class="row align-self-center">
     <div class="col-md-4">
-      <div class="card w-75">
-        <form action="?op=addProduct" method="post">
-          Naam:<br>
-          <input type="text" name="name"><br>
-          Merk:<br>
-          <input type="text" name="brand"><br>
-          Plaatje bril:<br>
-          <input type="file" name="pic"><br>
-          Merk:<br>
-          <input type="text" name="brand"><br>
-          Beschrijving:<br>
-          <input type="text" name="specification"><br>
-          Prijs:<br>
-          <input type="text" name="price"><br>
-          Aantal in voorraad:<br>
-          <input type="text" name="qty"><br>
-          Product op uitverkoop:<br>
-          <select name="sale">
-            <option value="true">Ja</option>
-            <option value="false">Nee</option>
-          </select>
-          Procent korting: (als van toepassing)<br>
-          <input type="text" name="salePercent"><br>
-        </form>
-      </div>
+      <input type="text" class="form-control" name="name" placeholder="First name">
+    </div>
+    <div class="col-md-4">
+      <input type="text" class="form-control" name="brand" placeholder="Last name">
+    </div>
+    <div class="col-md-4">
+      <input type="file" class="form-control" name="pic" placeholder="Picture">
+    </div>
+    <div class="col-md-4">
+      <input type="text" class="form-control" name="description" placeholder="Description">
+    </div>
+    <div class="col-md-4">
+      <input type="text" class="form-control" name="price" placeholder="Prijs">
+    </div>
+    <div class="col-md-4">
+      <input type="text" class="form-control" name="qty" placeholder="Aantal producten">
+    </div>
+    <div class="col-md-4">
+      <select class="custom-select" id="inputGroupSelect01" name="sale">
+        <option selected>Korting?</option>
+        <option value="true">Ja</option>
+        <option value="false">Nee</option>
+      </select>
+    </div>
+    <div class="col-md-4">
+      <input type="text" class="form-control" name="salePercent" placeholder="First name">
     </div>
   </div>
+  <button type="submit" class="btn btn-primary" style="margin-top:20px;">Submit</button>
+</form>
 </div>
 <?php
 include_once "view/footer.php";
