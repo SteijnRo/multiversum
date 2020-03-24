b0d97f730c685096a39375275a0992c4939ef833
Steijn Roosendaal <534812@edu.rocmn.nl>
2020-03-24 23:10:13 +0100

    yes

--- controller/Controller.php
+++ controller/Controller.php
@@ -2,76 +2,79 @@
 require_once 'model/ProductLogic.php';
 
 class Controller {
-    public function __construct() {
-        $this->ProductLogic =  new ProductLogic();
-    }
-    public function __destruct() {}
-    public function handleRequest() { 
-      try {
-        $op = isset($_REQUEST['op'])?$_REQUEST['op']:null;
-        switch($op) {
-          case 'main':
-            $this->collectReadProducts();
-          break;
-          case 'details':
-            $this->collectReadProduct($_GET["id"]);
-          break;
-          case 'contact':
-            $this->collectReadContacts();
-          break;
-          case 'insertProducts':
-            $this->collectInsertFormProducts();
-          break; 
-          case 'addProduct':
-            $this->collectCreateProduct();
-          break;
-          case 'updateGoggleForm':
-            $this->collectReadProductUpdate($_GET["id"]);
-          break;
-          case 'updateGoggle':
-            $this->collectUpdateGoggle($_POST, $_FILES);
-          break;
-          case 'sendEmail':
-            $this->collectSendEmail($_POST);
-          break;
-          case 'updateContactForm':
-            $this->collectUpdateContactData($_POST);
-          break;
-          case 'updateContact':
-            $this->collectUpdateContact($_POST);
-          break;
-          case 'buy':
-            $this->collectReadBuyForm();
-          break;
-          case 'placeOrder':
-            $this->collectCreateOrder();
-          break;
-          case 'updateCopyrightForm':
-            $this->collectUpdateCopyrightData($_POST);
-          break;
-          case 'updateCopyright':
-            $this->UpdateCopyright($data);
-          break;
-          case 'privacy':
-            $this->collectReadPrivacy();
-          break;
-          case 'copyright':
-            $this->collectReadCopyright();
-          break;
-          case 'algemenevoorwaarden':
-            $this->collectReadAlgemeneVoorwaarden();
-          break;
-          case 'cookies':
-            $this->collectReadCookies();
-          break;
-          default:
-            $this->collectReadProducts();
-          break;
-        }
-      } catch (Exception $e) {
-        throw $e;
+  public function __construct() {
+      $this->ProductLogic =  new ProductLogic();
+  }
+  public function __destruct() {}
+  public function handleRequest() { 
+    try {
+      $op = isset($_REQUEST['op'])?$_REQUEST['op']:null;
+      switch($op) {
+        case 'main':
+          $this->collectReadProducts();
+        break;
+        case 'details':
+          $this->collectReadProduct($_GET["id"]);
+        break;
+        case 'contact':
+          $this->collectReadContacts();
+        break;
+        case 'insertProducts':
+          $this->collectInsertFormProducts();
+        break; 
+        case 'addProduct':
+          $this->collectCreateProduct();
+        break;
+        case 'updateGoggleForm':
+          $this->collectReadProductUpdate($_GET["id"]);
+        break;
+        case 'updateGoggle':
+          $this->collectUpdateGoggle($_POST, $_FILES);
+        break;
+        case 'sendEmail':
+          $this->collectSendEmail($_POST);
+        break;
+        case 'updateContactForm':
+          $this->collectUpdateContactData($_POST);
+        break;
+        case 'updateContact':
+          $this->collectUpdateContact($_POST);
+        break;
+        case 'buy':
+          $this->collectReadBuyForm();
+        break;
+        case 'placeOrder':
+          $this->collectCreateOrder();
+        break;
+        case 'orderOverview':
+          $this->collectReadOrders();
+        break;
+        case 'updateCopyrightForm':
+          $this->collectUpdateCopyrightData($_POST);
+        break;
+        case 'updateCopyright':
+          $this->UpdateCopyright($data);
+        break;
+        case 'privacy':
+          $this->collectReadPrivacy();
+        break;
+        case 'copyright':
+          $this->collectReadCopyright();
+        break;
+        case 'algemenevoorwaarden':
+          $this->collectReadAlgemeneVoorwaarden();
+        break;
+        case 'cookies':
+          $this->collectReadCookies();
+        break;
+        default:
+          $this->collectReadProducts();
+        break;
       }
+    } catch (Exception $e) {
+      throw $e;
     }
+  }
   public function collectCreateProduct() {
     $content = $this->ProductLogic->createProduct($_POST, $_FILES);
     include_once 'view/insertProducts.php';
@@ -90,6 +93,11 @@ class Controller {
   public function collectReadProducts() { 
     $content = $this->ProductLogic->readProducts();
     include_once 'view/main.php';
+  }
+
+  public function collectReadOrders() { 
+    $content = $this->ProductLogic->readOrders();
+    include_once 'view/readOrders.php';
   }
 
   public function collectReadPrivacy() {
---
+++ view/readOrders.php
@@ -0,0 +1,46 @@
+<?php 
+include 'view/header.php';
+$orders = $content["result"]["orders"];
+$products = $content["result"]["products"];
+?>
+<div class="album py-5" id="main">
+  <div class="container">
+    <?php
+    echo '
+    <div class="row">
+      <div class="col-md-8">
+        <div class="card w-75">
+          <div class="card-body">
+            <table class="orderOverviewTable">
+              <tr>
+                <th>Order #</th>
+                <th>Verkocht product</th>
+                <th>Totaal prijs inclusief BTW</th>
+              </tr>
+            ';
+            for ($i = 0; $i < count($orders); $i++) {
+              echo '
+              <tr>
+              ';
+              for ($j = 0; $j < count($products[$i]); $j++) {
+                echo '
+                <th>' . $orders[$i]["id"] . '</th>
+                <th>' . $products[$i][$j]["name"] . '</th>
+                <th>' . $products[$i][$j]["price"] . '</th>
+                ';
+              }
+              echo '
+              </tr>
+              ';
+            }
+    echo '
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    ';
+    ?>
+  </div>
+</div>
+<?php include 'view/footer.php'; ?>
