b333e9ec82fe4fb32f9a8433e381f29f43ce1b82
Steijn Roosendaal <534812@edu.rocmn.nl>
2020-04-08 13:46:43 +0200

    lolgotem

--- view/main.php
+++ view/main.php
@@ -29,32 +29,34 @@ $products = $content['result'];
         if (!isset($products[$startProducts])) {
           break;
         }
-        echo '
-        <div class="col-md-4">
-          <div class="card w-75">
-            <div class="card-img-outer">
-              <div class="card-img">
-                <a href="?op=details&id=' . $products[$startProducts]["id"] . '"><img class="card-img-top" id="product-img" src="./view/assets/media/' . $products[$startProducts]["pic"] . '" alt="pic" height="300px"></a>
+        if ($products[$startProducts]["archive"] == 0) {
+          echo '
+          <div class="col-md-4">
+            <div class="card w-75">
+              <div class="card-img-outer">
+                <div class="card-img">
+                  <a href="?op=details&id=' . $products[$startProducts]["id"] . '"><img class="card-img-top" id="product-img" src="./view/assets/media/' . $products[$startProducts]["pic"] . '" alt="pic" height="300px"></a>
+                </div>
+              </div>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="?op=details&id=' . $products[$startProducts]["id"] . '">' . $products[$startProducts]["name"] . '</a>
+                </h4>
+                <h5>€' . $products[$startProducts]["price"] . '</h5>
+                <p class="card-text">' . $products[$startProducts]["description"] . '</p>
+              </div>
+              <div class="card-footer">
+                <form action="?op=buy" method="post">
+                  <input type="hidden" name="productPrice" value="' . $products[$startProducts]["price"] . '">
+                  <input type="hidden" name="productName" value="' . $products[$startProducts]["name"] . '">
+                  <input type="hidden" name="productID" value="' . $products[$startProducts]["id"] . '">
+                  <input type="submit" value="Koop nu">
+                </form>
               </div>
             </div>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="?op=details&id=' . $products[$startProducts]["id"] . '">' . $products[$startProducts]["name"] . '</a>
-              </h4>
-              <h5>€' . $products[$startProducts]["price"] . '</h5>
-              <p class="card-text">' . $products[$startProducts]["description"] . '</p>
-            </div>
-            <div class="card-footer">
-              <form action="?op=buy" method="post">
-                <input type="hidden" name="productPrice" value="' . $products[$startProducts]["price"] . '">
-                <input type="hidden" name="productName" value="' . $products[$startProducts]["name"] . '">
-                <input type="hidden" name="productID" value="' . $products[$startProducts]["id"] . '">
-                <input type="submit" value="Koop nu">
-              </form>
-            </div>
           </div>
-        </div>
-        ';
+          ';
+        }
         // rating
       }
       echo '</div>';
